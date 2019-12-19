ba989f1b2dee9ebc329ed2a5af6b38c8cbedc0ad
mhsajjadhossain <mail.mhsajjad@gmail.com>
2019-12-19 15:57:53 +0600

    this is the alfa version 1 update

--- script.js
+++ script.js
@@ -6,37 +6,41 @@ $(function () {
     $('.filter-menu').children().addClass('filter-item');
     $('.mix-wrap').children().addClass('filter-mix');
     $('.filter-mix').css({
-        'transition':'all .3s linear',
+        'transition': 'all .3s linear',
     });
-    // var btnData = $('.filter-menu .filter-item').attr('data-target');
-    
+    $('.mix-wrap').css({
+        'overflow': 'hidden',
+    });
+
+
 
     var i;
     var x = $('.filter-item').toArray();
     for (i = 0; i < x.length; i++) {
-        $(x[i]).on('click',function(e){
+        $(x[i]).on('click', function (e) {
             e.preventDefault();
             var attrdata = $(e.target).attr('data-category-name');
             console.log(attrdata);
 
             var nam = $(this).attr('data-category-name');
             $('.mix-wrap > .filter-mix').css({
-                'transform':'scale(0)',
+                'transform': 'scale(0)',
+                'position': 'absolute',
             });
-
-            $('.mix-wrap > div.filter-mix[data-category-name="'+nam+'"]').css({
-                'transform':'scale(1)',
+            $('.mix-wrap > div.filter-mix[data-category-name="' + nam + '"]').css({
+                'position': 'relative',
+                'transform': 'scale(1)',
             });
 
         });
-
     }
 
     var all = '*';
-    $('.filter-menu > .filter-item[data-category-name="'+all+'"]').on('click',function(){
+    $('.filter-menu > .filter-item[data-category-name="' + all + '"]').on('click', function () {
         console.log('hello');
         $('.mix-wrap > .filter-mix').css({
-            'transform':'scale(1)',
+            'position': 'relative',
+            'transform': 'scale(1)',
         });
     });
 
@@ -46,10 +50,10 @@ $(function () {
 
 
 
-    
 
-   
-    
+
+
+
 
 
 })
